dfab1258194882749a5cfea3946467cfe22e0a6f
Ryan Ridden <rri38@MacBook-Pro-2.canterbury.ac.nz>
2022-09-05 12:23:23 +1200

    make master flat

--- pouakai/calibration_masters.py
+++ pouakai/calibration_masters.py
@@ -87,7 +87,8 @@ def make_master_darks(save_location = '/home/phys/astronomy/rri38/moa/data/maste
 			masters.to_csv('cal_lists/master_dark_list.csv',index=False)
 
 
-def get_master_dark(jd,exptime,chip,strict=True,tol=10):
+
+def get_master_dark(jd,exptime,chip,strict=True,tol=1):
 	"""
 	ytdhgvj
 	"""
@@ -267,133 +268,122 @@ def new_make_master_flats(save_location = '/home/phys/astronomy/rri38/moa/data/m
 	# make save_location an environment variable
 	flat_list = pd.read_csv('cal_lists/flat_list.csv')
 	masters = pd.read_csv('cal_lists/master_flat_list.csv')
-	if redo_bad:
-		masters = cut_bad_reductions(masters)
-	names = split_names(flat_list['name'].values)
+
+	ind = (flat_list['field'] == 'flat_round') & (flat_list['note'] is not 'over')
+	flat_list = flat_list.iloc[ind]
+	flat_list['band'] = flat_list['band'].str.strip()
+	times = flat_list['jd'].values.astype(int)
+	names = ('F' + times.astype(str) + '_' + time_frame + 'd_' + flat_list['band'].values
+			 + '_' + flat_list['chip'].values.astype(str))
+
 	all_names = set(names)
 	master_names = set(split_names(masters['name'].values))
-	print(len(all_names))
 	new = all_names ^ master_names
 	new = list(new)
-	print(new)
-	print(len(new))
+	print('Number of new entries: ',len(new))
 	new.sort(reverse=True)
 	for i in range(len(new)):
 		print(str(i) + ' of ' + str(len(new)))
 		entry = {}
 		n = new[i]
-		ind = np.array(names) == n
-		all_chips = flat_list.iloc[ind]
+		t = int(n[1:].split('_')[0])
+		c = int(n.split('_')[-1])
+		b = n.split('_')[2]
 
-		dark_get = True
-		for j in range(10):
-			j += 1
-			chip_ind = all_chips['chip'].values == j
-			chip = all_chips.iloc[chip_ind]
-			chip_files = chip['filename'].values
-			master = []
-			for file in chip_files:
-				hdu = fits.open(file)[0]
-				header = hdu.header
-				data = hdu.data.astype(float)
+		tind = (t - times > 0) & (t - times < time_frame)
+		cind = flat_list['chip'].values == c
+		bind = flat_list['band'].values == b
 
-				saturations = (data > 50000).flatten()
-				# if more than 10% of pixels are saturated, set array to nan
-				if sum(saturations) > len(saturations) * 0.1:
-					print('image ', file, ' is saturated')
-					data = data * np.nan
+		ind = tind & cind & bind
 
-				master += [data]
-			master = np.array(master)
-			if verbose:
-				print('Used ',len(master),' images in median')
-			# get dark frame
-			if dark_get:
-				fname, tdiff = get_master_dark(chip['jd'].values[0], chip['exptime'].values[0], j)
-				dark_name = fname.split(str(j)+'.fits.gz')[0]
-				d_tdiff = tdiff
-				dark_get = False
-			else:
-				if dark_name != 'none':
-					fname = dark_name + str(j) + '.fits.gz'
-					tdiff = d_tdiff
-			if verbose:
-				print('using dark frame ',fname)
-				print('time difference ',tdiff)
+		files = flat_list['filename'].values[ind]
+		exptimes = flat_list['exptime'].values[ind]
+
+
+		master_arr = []
+		darks = []
+		for j in range(len(files)):
+			hdu = fits.open(files[j])[0]
+			header = hdu.header
+			data = hdu.data.astype(float)
+
+			saturations = (data > 50000).flatten()
+			# if more than 10% of pixels are saturated, set array to nan
+			if sum(saturations) > len(saturations) * 0.1:
+				print('image ', file, ' is saturated')
+				data = data * np.nan
+			master_arr += [data]
+
+			fname, tdiff = get_master_dark(t, exptimes, c)
 			try:
-				dark = fits.open(fname)[0].data
-				master = master - dark
-
+				darks += [fits.open(fname)[0].data]
 			except:
-				m = '!!! Warning: No dark found !!!'
-				print(m)
-				tdiff = -999
-			
-			mas = np.nanmedian(master,axis=0)
-			std = np.nanstd(master,axis=0)
-			time = np.nanmean(chip['jd'])
-			header['JDSTART'] = time 
-			header['MASTER'] = True
-			phdu = fits.PrimaryHDU(data = mas, header = header)
-			ehdu = fits.ImageHDU(data = std, header = header)
-			hdul = fits.HDUList([phdu, ehdu])
+				darks += [data * np.nan]
 
+		master_arr = np.array(master_arr)
+		darks = np.array(darks)
+		master_arr = master_arr - darks
 
-			letter = file.split('-')[3]
-			base_name = file.split('/')[-1].split('.')[0].replace(letter,'m')
-			save_name = save_location + base_name + '.fits'
-			print('saving')
-			hdul.writeto(save_name,overwrite=True)
-			compress = 'gzip -f ' + save_name
-			os.system(compress)
-			print('saved')
-			entry['name'] = base_name
+		mas = np.nanmedian(master_arr,axis=0)
+		std = np.nanstd(master_arr,axis=0)
+		
+		header['JDSTART'] = t 
+		header['MASTER'] = True
+		phdu = fits.PrimaryHDU(data = mas, header = header)
+		ehdu = fits.ImageHDU(data = std, header = header)
+		hdul = fits.HDUList([phdu, ehdu])
 
-			entry['band'] = header['COLOUR']
-			entry['chip'] = header['CHIP']
-			entry['exptime'] = header['EXPTIME']
-			entry['jd'] = time
-			entry['date'] = header['DATE-OBS']
-			entry['filename'] = save_name + '.gz'
-			entry['dark_file'] = fname 
-			entry['time_diff'] = tdiff
-			entry['nimages'] = len(master)
+		save_name = save_location + n + '.fits'
+		print('saving')
+		hdul.writeto(save_name,overwrite=True)
+		compress = 'gzip -f ' + save_name
+		os.system(compress)
+		print('saved')
+		entry['name'] = n
 
-			field = header['FIELD']
-			if 'flat_round' in field:
-				flat_type = 'dome'
-			else:
-				flat_type = 'sky'
-			entry['field'] = field
-			entry['flat_type'] = flat_type
+		entry['band'] = header['COLOUR']
+		entry['chip'] = header['CHIP']
+		entry['exptime'] = header['EXPTIME']
+		entry['jd'] = t
+		entry['date'] = header['DATE-OBS']
+		entry['filename'] = save_name + '.gz'
+		entry['nimages'] = len(master_arr)
 
-			if (np.nanmedian(mas) < 15000) | (np.nansum(mas) <= 0):
+		field = header['FIELD']
+		if 'flat_round' in field:
+			flat_type = 'dome'
+		else:
+			flat_type = 'sky'
+		entry['field'] = field
+		entry['flat_type'] = flat_type
+
+		if (np.nanmedian(mas) < 15000) | (np.nansum(mas) <= 0):
+			note = 'bad'
+		else:
+			if (len(master) < 2) & (flat_type == 'dome'):
 				note = 'bad'
 			else:
-				if (len(master) < 2) & (flat_type == 'dome'):
-					note = 'bad'
-				else:
-					note = 'good'
-			entry['note'] = note
-			
-			field = header['FIELD']
-			if 'flat_round' in field:
-				flat_type = 'dome'
-			else:
-				flat_type = 'sky'
-			entry['field'] = field
-			entry['flat_type'] = flat_type
-
-			if verbose:
-				print('Done ', base_name)
+				note = 'good'
+		entry['note'] = note
 		
-			masters = masters.append(entry, ignore_index=True)
-			masters.to_csv('cal_lists/master_flat_list.csv',index=False)
+		field = header['FIELD']
+		if 'flat_round' in field:
+			flat_type = 'dome'
+		else:
+			flat_type = 'sky'
+		entry['field'] = field
+		entry['flat_type'] = flat_type
+
+		if verbose:
+			print('Done ', n)
+	
+		masters = masters.append(entry, ignore_index=True)
+		masters.to_csv('cal_lists/master_flat_list.csv',index=False)
 
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
 	make_master_darks(verbose=True)
 	print('!!! Finished darks !!!')
-	make_master_flats(verbose=True)
+	new_make_master_flats(verbose=True)
 	print('!!! Finished flats !!!')
