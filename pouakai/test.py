70f51ea812fb9996f0737d58a39f8c95660de045
Cheerfuluser <ryanridden@gmail.com>
2024-06-10 16:12:58 +1200

    update

--- pouakai/test.py
+++ pouakai/test.py
@@ -1,3 +1,8 @@
-from core import pouakai
+from pouakai import consume_moa
+from glob import glob 
+import numpy as np
 
-pouakai(file='../dev/A6309-12932zephyr-R-3.fit.gz',savepath='/home/phys/astronomy/rri38/test/')
+files = glob('/home/phys/astro8/MJArchive/octans/**/EPS*')
+files = np.array(files)
+consume_moa(files=files,savepath='/home/phys/astronomy/rri38/fli/data/reduced/',cores=1,update_cals=False,overwrite=False,time_tolerence=1000)
+#consume_moa(files=files,savepath='/home/phys/astronomy/rri38/fli/data/reduced/',cores=1,update_cals=False,overwrite=True)
